e1bc587a365fe4996210de699a5dd2e0b0449f70
Menchu Marín <menchu.marin@beezy.net>
2025-01-13 11:49:37 +0100

    update url to deployed server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "https://parlem-db-knhc.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
